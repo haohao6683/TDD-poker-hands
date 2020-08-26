d3d7c3e66451df2e6082f66e493ee405a1bde4e7
LISH5 <727904507@qq.com>
2020-08-26 15:23:57 +0800

    [Poker][Shao & Draymond] feature: implement should_return_true_when_getStraight_given_straight

--- src/main/java/com/example/PokerCardGroup.java
+++ src/main/java/com/example/PokerCardGroup.java
@@ -6,15 +6,21 @@ public class PokerCardGroup {
     private final List<PokerCard> pokerCardList;
     private PokerCard highCard;
     private boolean isFlush = true;
+    private boolean isStraight = true;
 
     public PokerCardGroup(List<PokerCard> pokerCardList) {
         this.pokerCardList = pokerCardList;
         findHighCard();
         checkFlush();
+        checkStraight();
     }
 
     public PokerCard getHighCard() {
         return highCard;
+    }
+
+    public boolean isStraight() {
+        return isStraight;
     }
 
     public boolean isFlush() {
@@ -31,6 +37,15 @@ public class PokerCardGroup {
         for (PokerCard pokerCard : pokerCardList) {
             if (!pokerCard.getSuit().equals(suit)) {
                 isFlush = false;
+                break;
+            }
+        }
+    }
+
+    private void checkStraight() {
+        for(int i = 0; i < pokerCardList.size() - 1; i++){
+            if(!pokerCardList.get(i).getValue().equals(pokerCardList.get(i+1).getValue() - 1) ){
+                isStraight = false;
                 break;
             }
         }
