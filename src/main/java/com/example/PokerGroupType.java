cb8c067d8c7ea7f24faab60898c68cea9bce9829
LISH5 <727904507@qq.com>
2020-08-28 11:42:59 +0800

    [Poker][Shao & Draymond] refactor: change Integer to int

--- src/main/java/com/example/PokerGroupType.java
+++ src/main/java/com/example/PokerGroupType.java
@@ -1,20 +1,20 @@
 package com.example;
 
 public interface PokerGroupType {
-    Integer HIGH_CARD = 1;
-    Integer PAIR = 2;
-    Integer TWO_PAIRS = 3;
-    Integer THREE_OF_A_KIND = 4;
-    Integer STRAIGHT = 5;
-    Integer FLUSH = 6;
-    Integer FULL_HOUSE = 7;
-    Integer FOUR_OF_A_KIND = 8;
-    Integer STRAIGHT_AND_FLUSH = 9;
+    int HIGH_CARD = 1;
+    int PAIR = 2;
+    int TWO_PAIRS = 3;
+    int THREE_OF_A_KIND = 4;
+    int STRAIGHT = 5;
+    int FLUSH = 6;
+    int FULL_HOUSE = 7;
+    int FOUR_OF_A_KIND = 8;
+    int STRAIGHT_AND_FLUSH = 9;
 
-    Integer FOUR_REPEATING_CARD = 4;
-    Integer THREE_REPEATING_CARD = 3;
-    Integer TWO_REPEATING_CARD = 2;
+    int FOUR_REPEATING_CARD = 4;
+    int THREE_REPEATING_CARD = 3;
+    int TWO_REPEATING_CARD = 2;
 
-    Integer TWO_PACK_REPEATING = 2;
-    Integer ONE_PACK_REPEATING = 1;
+    int TWO_PACK_REPEATING = 2;
+    int ONE_PACK_REPEATING = 1;
 }
